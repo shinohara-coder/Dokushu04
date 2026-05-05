8d47266089c1971e8e8a38d5ece15d2b83ec4d07
hiroki <hiroki10484962@gmail.com>
2026-05-05 22:15:58 +0900

    P467まで

--- Pro02/Program.cs
+++ Pro02/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Runtime.Serialization;
-using static System.Console;
-using static System.Math;
 
 namespace Dokushu04
 {
@@ -9,7 +7,27 @@ namespace Dokushu04
     {
         static void Main(string[] args)
         {
-            throw new MyAppException("例外発生！");
+            Exception ex1 = new MyAppException();
+
+            Exception ex2 = new MyAppException("エラーが発生しました！");
+
+            Exception inner = new FormatException("元の数字変換エラー");
+            Exception ex3 = new MyAppException("計算中に問題が発生しました。", inner);
+
+            var info = new SerializationInfo(typeof(MyAppException), new FormatterConverter());
+            var context = new StreamingContext();
+            info.AddValue("ClassName", "MyAppException");
+            info.AddValue("Message", "シリアル化経由のメッセージ");
+            info.AddValue("StackTraceString", null);
+            info.AddValue("ExceptionMethod", null);
+            //info.AddValue("HResult", 0);
+            info.AddValue("Source", null);
+            info.AddValue("InnerException", null);
+
+            Exception ex4 = new MyAppException(info, context);
+
+            Console.WriteLine("例外を投げます。");
+            throw ex4;
         }
     }
 
---
+++ Pro04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Pro04.hoge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var name in Enum.GetValues(typeof(Season)))
+            {
+                Console.WriteLine($"{(int)name} : {name}");
+            }
+        }
+    }
+
+    internal enum Season { 
+        Spring,
+        Summer,
+        Autum,
+        Winter,
+        All = Spring + Summer + Autum + Winter,
+    }
+
+}
