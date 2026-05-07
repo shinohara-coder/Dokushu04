cb6f8cc4736dbfa15f1f3a913913cf2294ea23f2
hiroki <hiroki10484962@gmail.com>
2026-05-07 21:07:13 +0900

    P496から

---
+++ Pro05/MyPatial1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro05
+{
+    internal partial class MyPartialMethod
+    {
+        static partial void Log()
+        {
+            Console.WriteLine("ログを記録しました。");
+        }
+    }
+}
---
+++ Pro05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Pro05
+{
+    internal class MyClass
+    {
+        string str1 = "包含・インスタンス";
+        static string str2 = "包含・静的";
+
+        public void Show()
+        {
+            MyHelper h = new MyHelper();
+            h.Show();
+            Console.WriteLine(h.str1);
+            Console.WriteLine(MyHelper.str2);
+        }
+        internal class MyHelper
+        {
+            internal string str1 = "入れ子・インスタンス";
+            internal static string str2 = "入れ子・静的";
+
+            public void Show()
+            {
+                MyClass c = new MyClass();
+                Console.WriteLine(c.str1);
+                Console.WriteLine(MyClass.str2);
+            }
+        }
+
+        
+    }
+
+    internal partial class MyPartialMethod
+    {
+        static partial void Log();
+        static void Main(string[] args)
+        {
+            Log();
+        }
+    }
+
+}
