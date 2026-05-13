ce6b41fc7827388c4dc7349cd75d12c9960426ac
hiroki <hiroki10484962@gmail.com>
2026-05-13 21:16:27 +0900

    P545まで

--- Pro12/Program.cs
+++ Pro12/Program.cs
@@ -5,39 +5,26 @@ using System.Diagnostics.Metrics;
 
 namespace Pro10
 {
-    delegate void OutputProcess(string str);
-    internal class DelegeteMulti
+    internal class DelegateAnonymous
     {
-        void ArrayWalk(string[] data, OutputProcess output)
+        void ArrayWalk(string[] data, Func<string, string> output)
         {
             foreach (var value in data)
             {
-                output(value);
+                Console.WriteLine(output(value));
             }
-        }
-
-        static void AddQuote(string data)
-        {
-            Console.WriteLine($"[{data}]");
-        }
-
-        static void Front4(string data)
-        {
-            Console.WriteLine(data.Substring(0, 4));
         }
 
         static void Main(string[] args)
         {
             string[] data = { "あかまきがみ", "あおまきがみ", "きまきがみ" };
-            var dm = new DelegeteMulti();
-            OutputProcess? proc = AddQuote;
-            proc += Front4;
-            dm.ArrayWalk(data, proc);
+            var dm = new DelegateAnonymous();
+            dm.ArrayWalk(data, delegate (string d)
+            {
+                return $"[{d}]";
+            });
 
-            Console.WriteLine("----------------");
-
-            proc -= Front4;
-            dm.ArrayWalk(data, proc);
+            dm.ArrayWalk(data, delegate (string d) { return $"[{d.Substring(0, 4)}]"; });
         }
     }
 
