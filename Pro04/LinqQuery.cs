d8d1f42673c05f7d831de866088c52db66f0d71c
hiroki <hiroki10484962@gmail.com>
2026-05-19 21:56:43 +0900

    P562途中から

---
+++ Pro04/LinqQuery.cs
@@ -0,0 +1,24 @@
+﻿using Pro04.hoge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro04
+{
+    
+    
+    internal class LinqQuery
+    {
+        static void Main(string[] args)
+        {
+            var bs = AppTables.Books
+                    .Where(b => b.Price < 3800)
+                    .Select(b => new { Title = b.Title, Price = b.Price });
+
+            foreach (var b in bs)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
