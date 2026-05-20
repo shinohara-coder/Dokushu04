d2cc03ae15c30bcdc5b10034aeb52a3b8658ff0e
hiroki <hiroki10484962@gmail.com>
2026-05-21 07:01:56 +0900

    P567まで

--- Pro04/LinqQuery.cs
+++ Pro04/LinqQuery.cs
@@ -12,11 +12,11 @@ namespace Pro04
         static void Main(string[] args)
         {
             //var bs = from b in AppTables.Books
-            //         where (new int[] { 3, 6 }.Contains(b.Published.Month))
+            //         where (2000 <= b.Price && b.Price <= 3500)
             //         select new { Title = b.Title, Price = b.Price, Published = b.Published };
 
             var bs = AppTables.Books
-                    .Where(b => new int[] {3, 6}.Contains(b.Published.Month))
+                    .Where(b => 2000 <= b.Price && b.Price <= 3500)
                     .Select(b => new { Title = b.Title, Price = b.Price, Published = b.Published });
 
             foreach (var b in bs)
