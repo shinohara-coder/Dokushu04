c8ef5d0dddc107c2bd2de5d4cd87783bfd402ba2
hiroki <hiroki10484962@gmail.com>
2026-05-26 22:04:08 +0900

    P574から

--- Pro04/LinqQuery.cs
+++ Pro04/LinqQuery.cs
@@ -5,24 +5,26 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Pro04
-{
-    
-    
+{   
     internal class LinqQuery
     {
         static void Main(string[] args)
         {
+            //var bs = from b in AppTables.Books
+            //         group b by b.Publisher;
+
             var bs = AppTables.Books
-                    .Where(b => b.Price > 10000)
-                    .OrderBy(b => b.Price)
-                    .FirstOrDefault();
+                    .GroupBy(b => b.Publisher,
+                        b => new { Title = b.Title, Price = b.Price});
 
-            //foreach (var b in bs)
-            //{
-            //    Console.WriteLine(b);
-            //}
-
-            Console.WriteLine(bs);
+            foreach (var b in bs)
+            {
+                Console.WriteLine($"[{b.Key}]");
+                foreach (var t in b)
+                {
+                    Console.WriteLine($"{t.Title} ({t.Price}円)");
+                }
+            }
         }
     }
 }
