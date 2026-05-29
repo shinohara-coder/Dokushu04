0677fbfae8e0ea6950ed0e96bea15901c3849675
H_Shinohara <shinohara@softcreate.net>
2026-05-29 09:42:45 +0900

    P585から

--- Pro05/Program.cs
+++ Pro05/Program.cs
@@ -5,42 +5,26 @@ namespace Pro05
 {
     internal class MyClass
     {
-        string str1 = "包含・インスタンス";
-        static string str2 = "包含・静的";
-
-        public void Show()
-        {
-            MyHelper h = new MyHelper();
-            h.Show();
-            Console.WriteLine(h.str1);
-            Console.WriteLine(MyHelper.str2);
-        }
-        internal class MyHelper
-        {
-            internal string str1 = "入れ子・インスタンス";
-            internal static string str2 = "入れ子・静的";
-
-            public void Show()
-            {
-                MyClass c = new MyClass();
-                Console.WriteLine(c.str1);
-                Console.WriteLine(MyClass.str2);
-            }
-        }
-
-        
-    }
-
-    internal partial class MyPartialMethod
-    {
-        static partial void Log();
         static void Main(string[] args)
         {
-            Console.WriteLine(default(int));
-            Console.WriteLine(default(double));
-            Console.WriteLine(default(string));
-            Console.WriteLine(default(MyClass));
+            var t1 = new Thread(Count);
+            var t2 = new Thread(Count);
+            var t3 = new Thread(Count);
+
+            t1.Start(1);
+            t2.Start(2);
+            t3.Start(3);
         }
+
+
+        static void Count(object? n)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"Thread{n}: {i}");
+            }
+        }
+        
     }
 
 }
