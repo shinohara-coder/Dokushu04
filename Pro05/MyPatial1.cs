8008b232db266a05c8bd5a6d9525b1d046a08a0b
hiroki <hiroki10484962@gmail.com>
2026-05-29 22:43:13 +0900

    P586から

--- Pro05/MyPatial1.cs
+++ Pro05/MyPatial1.cs
@@ -6,9 +6,6 @@ namespace Pro05
 {
     internal partial class MyPartialMethod
     {
-        static partial void Log()
-        {
-            Console.WriteLine("ログを記録しました。");
-        }
+        
     }
 }
--- Pro05/Program.cs
+++ Pro05/Program.cs
@@ -14,6 +14,12 @@ namespace Pro05
             t1.Start(1);
             t2.Start(2);
             t3.Start(3);
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine("すべての処理が終了しました。");
         }
 
 
