5613a3a5b80b213d96fd1e470b230565143810e2
H_Shinohara <shinohara@softcreate.net>
2026-06-01 10:56:33 +0900

    P50から

--- Pro12/Program.cs
+++ Pro12/Program.cs
@@ -7,24 +7,15 @@ namespace Pro10
 {
     internal class DelegateAnonymous
     {
-        void ArrayWalk(string[] data, Func<string, string> output)
-        {
-            foreach (var value in data)
-            {
-                Console.WriteLine(output(value));
-            }
-        }
+        
 
         static void Main(string[] args)
         {
-            string[] data = { "あかまきがみ", "あおまきがみ", "きまきがみ" };
-            var dm = new DelegateAnonymous();
-            dm.ArrayWalk(data, delegate (string d)
-            {
-                return $"[{d}]";
-            });
-
-            dm.ArrayWalk(data, delegate (string d) { return $"[{d.Substring(0, 4)}]"; });
+            Console.WriteLine(sizeof(int));
+            Console.WriteLine(sizeof(double));
+            Console.WriteLine(sizeof(bool));
+            Console.WriteLine(sizeof(long));
+            Console.WriteLine(sizeof(decimal));
         }
     }
 
