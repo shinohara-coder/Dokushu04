d7947811304f2ebbc49b404a7d4a0ae5489e4413
hiroki <hiroki10484962@gmail.com>
2026-06-04 20:30:00 +0900

    P91から

--- Pro12/Program.cs
+++ Pro12/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Diagnostics.Metrics;
+using System.Text;
 
 namespace Pro10
 {
@@ -11,9 +13,27 @@ namespace Pro10
 
         static void Main(string[] args)
         {
-            int i = 128;
-            sbyte b = (sbyte)i;
-            Console.WriteLine(b);
+            const string STR = "いろは";
+            var result = "";
+            Stopwatch sw1 = Stopwatch.StartNew();
+            for (int i = 0; i < 1e+5; i++)
+            {
+                result += STR;
+            }
+            sw1.Stop();
+            Console.WriteLine($"所要時間：{sw1.ElapsedMilliseconds}ミリ秒");
+
+            result = "";
+
+            var builder = new StringBuilder();
+            Stopwatch sw2 = Stopwatch.StartNew();
+            for (int i = 0; i < 1e+5; i++)
+            {
+                builder.Append(STR);
+            }
+            result = builder.ToString();
+            sw2.Stop();
+            Console.WriteLine($"所要時間：{sw2.ElapsedMilliseconds}ミリ秒");
         }
     }
 
