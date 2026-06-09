8e1f64d49c79114c2750e7769b62aa55de7b5cb9
H_Shinohara <shinohara@softcreate.net>
2026-06-09 09:04:09 +0900

    P178から

--- Pro01/Program.cs
+++ Pro01/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Globalization;
 using System.Security.AccessControl;
+using System.Text.RegularExpressions;
 using SelfCSharp.Chap09.Priority1;
 //using MyUtil = SelfCSharp.Chap09.Priority2.MyUtil;
 
@@ -11,8 +12,14 @@ namespace SelfCSharp.Chap09.Priority1
     {
         static void Main(string[] args)
         {
-            func(string.Format(
-                "カスタム(補完なし) :{0:#,###.###}", 123456123.789));
+            var str = "自宅の電話番号は、084-000-0000です。携帯は、080-0000-0000です。";
+            Regex rgx = new Regex(@"(\d{2,4})-(\d{2,4})-(\d{4})");
+            Match match = rgx.Match(str);
+
+            if (match.Success)
+            {
+                func($"位置:{match.Index} マッチ文字列:{match.Value}");
+            }
         }
 
         static void func(object? str)
