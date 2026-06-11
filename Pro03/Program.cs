a6aa3794fab1e99212a15d81ba10a350e45bb03e
H_Shinohara <shinohara@softcreate.net>
2026-06-11 09:06:32 +0900

    P185から

--- Pro03/Program.cs
+++ Pro03/Program.cs
@@ -9,22 +9,21 @@ namespace Pro03
     {
         static void Main(string[] args)
         {
-            var str = "自宅の電話番号は、084-000-0000です。携帯は、080-0000-0000です。";
-            var pattern = @"
-                (\d{2,4})  # 市外局番
-                -(\d{2,4}) # 市内局番
-                -(\d{4})   # 加入者番号
-                ";
+            var str = "仕事用はwings@example.comです。プライベート用はYAMA@example.comです。";
+            //var rgx = new Regex(@"([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(\.[a-z0-9-]+)*)", RegexOptions.IgnoreCase);
+            var rgx = new Regex(@"(?i)([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(\.[a-z0-9-]+)*)");
+            //var rgx = new Regex(@"(?i:([a-z0-9.!#$%&'*+/=?^_{|}~-]+)@([a-z0-9-]+(\.[a-z0-9-]+)*))");
 
-            var rgx = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
-            var match = rgx.Match(str);
-            if (match.Success)
+            var result = rgx.Matches(str);
+
+            foreach (Match m in result)
             {
-                Console.WriteLine($"位置：{match.Index} マッチ文字列：{match.Value}");
-                foreach (Group g in match.Groups)
+                Console.WriteLine(m.Value);
+                foreach (Group g in m.Groups)
                 {
                     Console.WriteLine(g.Value);
                 }
+                Console.WriteLine("-----------");
             }
         }
     }
