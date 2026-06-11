30d5536b1b466fe329d668d7237337a315b928c6
H_Shinohara <shinohara@softcreate.net>
2026-06-11 17:17:37 +0900

    Pro04でジェネリクスについて学習

--- Pro04/LinqQuery.cs
+++ Pro04/LinqQuery.cs
@@ -10,35 +10,7 @@ namespace Pro04
     {
         static void Main(string[] args)
         {
-            /*var bs = from b in AppTables.Books
-                     join r in AppTables.Reviews on b.Isbn equals r.Isbn
-                     select new
-                     {
-                         Title = b.Title,
-                         Reviewer = r.Name,
-                         Body = r.Body
-                     };*/
-
-
-            var bs = AppTables.Books
-                    .Join(
-                        AppTables.Reviews,
-                        b => b.Isbn,
-                        r => r.Isbn,
-                        (b, r) => new
-                        {
-                            Title = b.Title,
-                            Reviewer = r.Name,
-                            Body = r.Body
-                        }
-                    );
             
-            foreach (var b in bs)
-            {
-                Console.WriteLine($"「{b.Title}」({b.Reviewer})");
-                Console.WriteLine($"{b.Body}");
-                Console.WriteLine("-----------");
-            }
         }
     }
 }
