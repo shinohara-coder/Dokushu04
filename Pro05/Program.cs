f6a8dacf6c27e333f68360974203355554014bf0
H_Shinohara <shinohara@softcreate.net>
2026-06-12 09:03:47 +0900

    P191から

--- Pro05/Program.cs
+++ Pro05/Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace Pro05
 {
+    
     internal class MyClass
     {
+        static void ShowMatch(string msg, Regex reg)
+        {
+            var matches = reg.Matches(msg);
+            foreach (Match result in matches)
+            {
+                Console.WriteLine(result.Value);
+            }
+            Console.WriteLine("--------------------------");
+        }
         static void Main(string[] args)
         {
-            var msg = "仕事用はwings@example.comです。プライベート用はYAMA@example.comです。";
+            var reg1 = new Regex("いろ(?=はに)");
+            var reg2 = new Regex("いろ(?!はに)");
+            var reg3 = new Regex("(?<=。)いろ");
+            var reg4 = new Regex("(?<!。)いろ");
             
         }
     }
