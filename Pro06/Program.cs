57b89413374d31f1b1ad2c7fc9785e067317d1ed
hiroki <hiroki10484962@gmail.com>
2026-06-13 22:15:51 +0900

    p206から

--- Pro06/Program.cs
+++ Pro06/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
+using System.Globalization;
 
 namespace Pro06
 {
@@ -8,17 +9,22 @@ namespace Pro06
     {
         static void Main(string[] args)
         {
-            DateTime dt = default;
-            func(dt);
-            if (DateTime.TryParse("2026/6/12 21:25:49", out dt))
-            {
-                func(dt);
-            }
+            var dt1 = new DateTime(2026, 6, 13, 22, 3, 45);
+            var dt2 = new DateTime(1974, 6, 2, 6, 5, 52);
+            var sub = dt1.Subtract(dt2);
+            func(sub.ToString("c"));
+            func(sub.ToString(@"d\.h\:m\:s"));
         }
 
         private static void func(object? str)
         {
             Console.WriteLine(str);
+        }
+
+        private static void ShowTime(DateTime dt, CultureInfo cul)
+        {
+            func(dt.ToString("f", cul));
+            func(dt.ToString("ggyy/MM/dd dddd tt hh:mm:ss", cul));
         }
     }
 
