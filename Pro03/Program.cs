b9e1755f69e480427ee8a9f3bd74b87637df7a9a
hiroki <hiroki10484962@gmail.com>
2026-06-20 22:20:52 +0900

    P277から

--- Pro03/Program.cs
+++ Pro03/Program.cs
@@ -1,19 +1,29 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 using System.Text.RegularExpressions;
 using static System.Console;
 using static System.Math;
 
 namespace Pro03
 {
-    class Program
+    internal class StringLengthComparer : IComparer<string>
+    {
+       public int Compare(string? x, string? y)
+        {
+            return x.Length - y.Length;
+        }
+    }
+    
+    internal class Program
     {
         static void Main(string[] args)
         {
-            var sd = new Dictionary<string, string>()
+            var sd = new SortedDictionary<string, string>()
             {
                 ["Rose"] = "バラ",
                 ["SunFlower"] = "ヒマワリ",
-                ["Morning Glory"] = "あさがお"
+                ["Morning Glory"] = "あさがお",
+                ["Lily"] = "ユリ"
             };
 
             foreach (var key in sd.Keys)
