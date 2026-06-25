72173c72ed5b4419b18a02628eaeabd7ad198957
hiroki <hiroki10484962@gmail.com>
2026-06-25 22:00:52 +0900

    P366から

--- Pro16/Program.cs
+++ Pro16/Program.cs
@@ -10,11 +10,28 @@ using System.Text;
 
 namespace Pro16
 {
-    internal class PassReIn
+    internal class PassOUt
     {
+        public void GetMaxMin(int x, int y, out int max, out int min)
+        {
+            if (x >= y)
+            {
+                max = x;
+                min = y;
+            }
+            else
+            {
+                max = y;
+                min = x;
+            }
+        }
         static void Main(string[] args)
         {
-            
+            var a = new PassOUt();
+            //int resultMax, resultMin;
+            a.GetMaxMin(5, 3, out int resultMax, out int resultMin);
+            func(resultMax);
+            func(resultMin);
         }
 
         static void func(object? obj)
