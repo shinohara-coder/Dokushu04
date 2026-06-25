212aaa01d1c0360476c86b0f2508e3bf47b2bf9b
hiroki <hiroki10484962@gmail.com>
2026-06-26 07:06:50 +0900

    P337から

---
+++ Pro17/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Text;
+
+namespace Pro17
+{
+    internal class Reffor
+    {
+        static void Main(string[] args)
+        {
+            var result = 0;
+            var sum = 0;
+
+            for (ref var i = ref result; i < 100; i++)
+            {
+                sum += i;
+                if (sum > 1000)
+                {
+                    break;
+                }
+            }
+
+            func($"合計が1000を超えるのは、1～{result}を加算した時です。");
+        }
+
+        static void func(object? obj)
+        {
+            Console.WriteLine(obj.ToString());
+        }
+    }
+}
