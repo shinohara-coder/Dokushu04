d9e53f53a744baac7dd653fa99f8d34474e45b87
H_Shinohara <shinohara@softcreate.net>
2026-06-25 11:29:06 +0900

    新規プロジェクトを追加

---
+++ Pro16/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Text;
+
+namespace Pro16
+{
+    internal struct Coodinate
+    {
+        public double X;
+        public double Y;
+
+        public Coodinate(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    internal class PassReIn
+    {
+        public static void Show(in Coodinate c)
+        {
+            func($"[{c.X}, {c.Y}]");
+            //c.X = 10;
+        }
+        static void Main(string[] args)
+        {
+            Show(new Coodinate(10, 15));
+        }
+
+        static void func(object? obj)
+        {
+            Console.WriteLine(obj.ToString());
+        }
+    }
+}
