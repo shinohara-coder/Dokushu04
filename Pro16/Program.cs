f8d3412a1dbc980e03b372b4df72e5a5892f46a4
H_Shinohara <shinohara@softcreate.net>
2026-06-25 11:30:01 +0900

    ちょっと修正

--- Pro16/Program.cs
+++ Pro16/Program.cs
@@ -10,27 +10,11 @@ using System.Text;
 
 namespace Pro16
 {
-    internal struct Coodinate
-    {
-        public double X;
-        public double Y;
-
-        public Coodinate(double x, double y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-    }
     internal class PassReIn
     {
-        public static void Show(in Coodinate c)
-        {
-            func($"[{c.X}, {c.Y}]");
-            //c.X = 10;
-        }
         static void Main(string[] args)
         {
-            Show(new Coodinate(10, 15));
+            
         }
 
         static void func(object? obj)
