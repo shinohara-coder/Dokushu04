c5736c6126c2019c2b9f19c8f44c59ab41620884
H_Shinohara <shinohara@softcreate.net>
2026-06-29 09:06:01 +0900

    P390から

---
+++ Pro09/Program09.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
+
+namespace Pro08
+{
+    internal class MyParent
+    {
+        public MyParent(string childname)
+        {
+            Console.WriteLine($"{childname}の親です。");
+        }
+    }
+
+    internal class MyChild : MyParent
+    {
+        public MyChild(string childname) : base(childname)
+        {
+            Console.WriteLine($"子の{childname}です。");
+        }
+    }
+
+    internal class MySingleton
+    {   
+        static void Main(string[] args)
+        {
+            var c = new MyChild("小太郎");
+        }
+    }
+}
+
---
+++ Pro10/Program10.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
+
+namespace Pro10
+{
+    internal class MyParent
+    {
+        public MyParent(string childname)
+        {
+            Console.WriteLine($"{childname}の親です。");
+        }
+
+        ~MyParent()
+        {
+            Console.WriteLine("親のファイナライザー");
+        }
+    }
+
+    internal class MyChild : MyParent
+    {
+        public MyChild(string childname) : base(childname)
+        {
+            Console.WriteLine($"子の{childname}です。");
+        }
+
+        ~MyChild()
+        {
+            Console.WriteLine("このファイナライザー");
+        }
+    }
+
+    internal class MySingleton
+    {
+        static void Main(string[] args)
+        {
+            var c = new MyChild("小太郎");
+        }
+    }
+}
+
