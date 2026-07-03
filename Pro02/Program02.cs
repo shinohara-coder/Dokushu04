bae6a0d48d7dd04293b9d8ad973748c84a413b87
shima <t-shima@softcreate.net>
2026-07-03 14:23:34 +0900

    P412から

--- Pro02/Program02.cs
+++ Pro02/Program02.cs
@@ -10,28 +10,54 @@ using SelfCSharp.Chap09.Priority1;
 
 namespace SelfCSharp.Chap09.Priority1
 {
-        
+    internal class Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public virtual double GetArea()
+        {
+            return 0.0;
+        }
+    }
+
+    internal class Triangle : Figure
+    {
+        public Triangle(double width, double height) : base(width, height) { }
+
+        //public override double GetArea()
+        public new double GetArea()
+        {
+            return this.Width * this.Height / 2;
+        }
+    }
+
+    internal class Square : Figure
+    {
+        public Square(double width, double height)
+            : base(width, height) { }
+
+        //public override double GetArea()
+        public new double GetArea()
+        {
+            return this.Width * this.Height;
+        }
+    }
+
     internal class LambdaCapture
     {
-        internal class Figure
-        {
-            public double Width { get; set; }
-            public double Height { get; set; }
-
-            public Figure(double width, double height)
-            {
-                this.Width = width;
-                this.Height = height;
-            }
-
-            public virtual double GetArea()
-            {
-                return 0.0;
-            }
-        }
         static void Main(string[] args)
         {
-            
+            Figure t = new Triangle(10, 30);
+            Console.WriteLine(t.GetArea());
+            Figure s = new Square(10, 30);
+            Console.WriteLine(s.GetArea());
         }
 
         static void func(object? str)
