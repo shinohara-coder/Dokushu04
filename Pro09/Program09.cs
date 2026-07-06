8293753eddfd5bd52c6bba95c58bbbeb14cf5bc7
shima <t-shima@softcreate.net>
2026-07-06 16:39:41 +0900

    P442から

--- Pro09/Program09.cs
+++ Pro09/Program09.cs
@@ -7,27 +7,22 @@ using System.Runtime.CompilerServices;
 
 namespace Pro08
 {
-    internal class MyParent
-    {
-        public MyParent(string childname)
-        {
-            Console.WriteLine($"{childname}の親です。");
-        }
-    }
-
-    internal class MyChild : MyParent
-    {
-        public MyChild(string childname) : base(childname)
-        {
-            Console.WriteLine($"子の{childname}です。");
-        }
-    }
-
     internal class MySingleton
     {   
         static void Main(string[] args)
         {
-            var c = new MyChild("小太郎");
+            try
+            {
+                using (var sr = new StreamReader(@"C:\Users\sc_design\Desktop\sample\SelfCSharp\Chap09\OpeBool.cs"))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("ファイルが見つかりませんでした。");
+                Console.WriteLine(ex.StackTrace);
+            }
         }
     }
 }
