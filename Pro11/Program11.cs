bdd1381a37289df5f95f2b687e008c4510366ddb
shima <t-shima@softcreate.net>
2026-07-07 14:20:47 +0900

    P458から

--- Pro11/Program11.cs
+++ Pro11/Program11.cs
@@ -1,25 +1,36 @@
 ﻿using IronPython.Hosting;
+using Microsoft.Scripting;
 using System.Diagnostics;
 using System.IO.Pipes;
 using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
 
 namespace Pro10
 {
+    internal class MyAppException : Exception
+    {
+        public MyAppException() { }
+
+        public MyAppException(string message) : base(message)
+        {
+        }
+
+        public MyAppException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public MyAppException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+    
     internal class ArgParams
     {
         static void Main(string[] arngs)
         {
-            try
-            {
-                using (var sr = new StreamReader(@"C:\nothing.dat"))
-                {
-                    Console.WriteLine(sr.ReadToEnd());
-                }
-            }
-            catch (FileNotFoundException ex)
-            {
-                throw;
-            }
+            var i = int.MaxValue;
+            Console.WriteLine(++i);
+            throw new MyAppException("例外発生！");
         }
     }
 
--- Pro12/Program12.cs
+++ Pro12/Program12.cs
@@ -8,41 +8,22 @@ using System.Text;
 
 namespace Pro10
 {
-    internal class Person
-    {
-        public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
-
-        public virtual string Show()
-        {
-            return $"名前は{this.LastName}{this.FirstName}です。";
-
-        }
-    }
-
-    internal class BusinessPerson : Person
-    {
-        public override string Show()
-        {
-            return $"会社員の{this.LastName}{this.FirstName}です。";
-        }
-
-        public string Work()
-        {
-            return $"{this.LastName}{this.FirstName}は、働きます。";
-        }
-    }
-
     internal class TypeGetBasic
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person();
-            Console.WriteLine(p1.GetType());
-            Person p2 = new BusinessPerson();
-            Console.WriteLine(p2.GetType());
-            Type t = typeof(Person);
-            Console.WriteLine(t.FullName);
+            try
+            {
+                checked
+                {
+                    var i = int.MaxValue;
+                    Console.WriteLine(++i);
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("オーバーフロー発生");
+            }
         }
     }
 
