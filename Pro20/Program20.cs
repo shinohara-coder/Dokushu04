d758322740e53e3dc7c86274105f3808cdc9fbe1
hiroki <hiroki10484962@gmail.com>
2026-07-10 22:29:10 +0900

    P483から

--- Pro20/Program20.cs
+++ Pro20/Program20.cs
@@ -10,17 +10,31 @@ using System.Text;
 using Pro20.IntExtensions;
 
 namespace Pro20
-{   
+{
+    internal record Person
+    { 
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public int Age { get; init; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+    }
     internal class IteratorBasic
     {
         static void Main(string[] args)
         {
-            5.Times(() => func("Hello world!"));   
-        }
+            var p1 = new Person("太郎", "山田", 10);
+            var p2 = new Person("太郎", "山田", 10);
 
-        static void func(object? obj)
-        {
-            Console.WriteLine(obj.ToString());
+            Console.WriteLine(p1);
+            Console.WriteLine(p1 == p2);
+            //(string fname, string lname, int age) = p1;
+            //Console.WriteLine(lname);
         }
     }
 }
---
+++ Pro21/Program21.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Text;
+
+namespace Pro21
+{
+
+    internal class IteratorBasic
+    {
+        public record Person(string FirstName, string LastName, int Age);
+        static void Main(string[] args)
+        {
+            var p = new Person("太郎", "山田", 10);
+            var pc1 = p with { Age = 20 };
+            var pc2 = p with { };
+
+            Console.WriteLine(pc1);
+            Console.WriteLine(pc2);
+        }
+    }
+}
