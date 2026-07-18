a4ae740cb73b4c270ad1b2315b2874f906a384e2
hiroki <hiroki10484962@gmail.com>
2026-07-18 22:08:04 +0900

    P526から

--- Pro08/Program08.cs
+++ Pro08/Program08.cs
@@ -3,12 +3,41 @@ using static System.Math;
 
 namespace Pro08
 {   
+    internal class Coodinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static Coodinate operator +(in Coodinate c1, in Coodinate c2)
+        {
+            return new Coodinate()
+            {
+                X = c1.X + c2.X,
+                Y = c1.Y + c2.Y
+            };
+        }
+
+        //public static Coodinate operator +(Coodinate c, int x)
+        //{
+        //    return new Coodinate()
+        //    {
+        //        X = c.X + x,
+        //        Y = c.Y
+        //    };
+        //}
+
+        public override string ToString()
+        {
+            return $"X: {this.X} Y: {this.Y}";
+        }
+    }
     internal class MySingleton
     {
         static void Main(string[] args)
         {
-            WriteLine(Abs(-999));
-            WriteLine(Round(1.7320508));
+            var c1 = new Coodinate { X = 10, Y = 20 };
+            c1 += new Coodinate() { X = 15, Y = 25 };
+            Console.WriteLine(c1);
         }
     }
 }
--- Pro09/Program09.cs
+++ Pro09/Program09.cs
@@ -7,23 +7,40 @@ using System.Runtime.CompilerServices;
 
 namespace Pro08
 {
+    internal class Coodinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static Coodinate operator ++(Coodinate c)
+        {
+            return new Coodinate()
+            {
+                X = c.X + 1,
+                Y = c.Y + 1
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"X: {this.X} Y: {this.Y}";
+        }
+    }
     internal class MySingleton
     {   
         static void Main(string[] args)
         {
-            try
-            {
-                var f = new StreamReader(@"C:\nothing.dat");
-            }
-            catch (FileNotFoundException ex) when (ex.Message.Contains(".dat"))
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("存在しない.datファイルが指定されました。");
-            }
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            var a = new Coodinate() { X = 10, Y = 20 };
+            var b = ++a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine("----------------------");
+
+            var m = new Coodinate() { X = 10, Y = 20 };
+            var n = m++;
+            Console.WriteLine(m);
+            Console.WriteLine(n);
         }
     }
 }
