82dfc484c8f77b27931ec3d545e42102d1480c42
hiroki <hiroki10484962@gmail.com>
2026-07-22 22:00:42 +0900

    P550から

--- Pro22/Program22.cs
+++ Pro22/Program22.cs
@@ -1,42 +1,14 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Diagnostics.Metrics;
-using System.Numerics;
-using System.Text;
-using static Pro22.MyClass;
-
-namespace Pro22
+﻿namespace Pro22
 {
-
-    internal class MyClass
-    {
-        internal class MyHelper
-        {
-            public void Show()
-            {
-                Console.WriteLine("Nested Class is running!");
-            }
-        }
-
-        public void Run()
-        {
-            var helper = new MyHelper();
-            helper.Show();
-        }
-    }
     internal class IteratorBasic
     {
         static void Main(string[] args)
         {
-            var c = new MyClass();
-            c.Run();
+            int factor = 2;
 
-            var h = new MyHelper();
-            h.Show();
+            //Func<double, double> func = static x => x * factor;
+            Func<double, double> func = x => x * factor;
+            Console.WriteLine(func(16));
         }
     }
 }
