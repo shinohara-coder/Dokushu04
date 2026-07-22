158d3b49a8521790ac8ea8e400da466bdb7b459f
shima <t-shima@softcreate.net>
2026-07-23 08:56:27 +0900

    P551から

--- Pro23/Program23.cs
+++ Pro23/Program23.cs
@@ -4,21 +4,42 @@
     {
         private int _value;
 
-        LambdaMember() => Console.WriteLine("consutructor");
+        LambdaMember() => Console.WriteLine("constructor");
 
         public int Value
         {
             get => this._value;
-            set => this._value = Value;
+            set => this._value = value;
         }
 
         public DateTime Current => DateTime.Now;
 
+        public int Calculate() => this.Value * this.Value;
+
         public int this[int index] => this.Value * index;
+
+        public static bool operator true(LambdaMember e) => e.Value == 0;
+        public static bool operator false(LambdaMember e) => e.Value != 0;
 
         static void Main(string[] args)
         {
-            
+            var lm = new LambdaMember
+            {
+                Value = 2
+            };
+
+            Console.WriteLine(lm.Current);
+            Console.WriteLine(lm[3]);
+            Console.WriteLine(lm.Calculate());
+
+            if (lm)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
         }
     }
 }
