2aeeaf6a9b8becd0490002a0986a0d4157505a92
H_Shinohara <shinohara@softcreate.net>
2026-07-23 18:43:17 +0900

    P552から

--- Pro25/Program25.cs
+++ Pro25/Program25.cs
@@ -1,14 +1,10 @@
 ﻿namespace Pro25.hoge
 {
     internal partial class MyPartialMethod
-    {
-        static partial void Log();
-        static partial void Greet();
-        
+    {   
         static void Main(string[] args)
         {
-            Log();
-            Greet();
+            
         }
     }
 }
