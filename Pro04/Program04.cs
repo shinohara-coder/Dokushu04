2df577de733e1b58138c9948cea407e63811be5b
shima <t-shima@softcreate.net>
2026-07-24 08:55:46 +0900

    P557から

--- Pro04/Program04.cs
+++ Pro04/Program04.cs
@@ -1,40 +1,12 @@
 ﻿namespace SelfCSharp.Chap09.Priority1
 {
-    internal class Person
-    {
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-
-        public Person(string firstName, string lastName)
-        {
-            this.FirstName = firstName;
-            this.LastName = lastName;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (Object.ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            if (obj == null || this.GetType() != obj.GetType())
-            {
-                return false;
-            }
-
-            return obj is Person p &&
-                this.FirstName == p.FirstName &&
-                this.LastName == p.LastName;
-        }
-    }
     internal class LambdaCapture
     {
         static void Main(string[] args)
         {
-            var p = new Person("哲夫", "萩原");
-            var p2 = new Person("哲夫", "萩原");
-            Console.WriteLine(p.Equals(p2));
+            var list = new List<string> { "からすなぜ鳴くの", "からすは山に",
+                 "可愛い七つの", "子があるからよ"};
+            Console.WriteLine(list.TrueForAll(str => str.Length >= 7));
         }
     }
 }
--- Pro05/Program05.cs
+++ Pro05/Program05.cs
@@ -1,56 +1,16 @@
 ﻿namespace SelfCSharp.Chap09.Priority1
 {
-    internal class Person : IEquatable<Person>
-    {
-        public string FirstName { get; private set; }
-        //public string LastName { get; private set; }
-        public string LastName { get; set; }
-
-        public Person(string firstName, string lastName)
-        {
-            this.FirstName = firstName;
-            this.LastName = lastName;
-        }
-
-        //IEquatable<Person>インターフェイスの実装
-        public bool Equals(Person? other)
-        {
-            if (Object.ReferenceEquals(this, other))
-            {
-                return true;
-            }
-
-            if (other == null || this.GetType() != other.GetType())
-            {
-                return false;
-            }
-
-            return this.FirstName == other.FirstName && this.LastName == other.LastName;
-        }
-
-        // Objectクラスのオーバーライド
-        public override bool Equals(object? obj)
-        {
-            return this.Equals(obj as Person);
-        }
-
-        public override int GetHashCode()
-        {
-            return this.FirstName.GetHashCode() ^ this.LastName.GetHashCode();
-        }
-    }
     internal class LambdaCapture
     {
         static void Main(string[] args)
         {
-            var p = new Person("太郎", "今宮");
-            Console.WriteLine(p.GetHashCode());
-            var d = new Dictionary<Person, int>();
-            var old_p = p;
-            d.Add(p, 10);
-            //p.LastName = "柱谷";
-            Console.WriteLine(p.GetHashCode());
-            Console.WriteLine(d[old_p]);
+            var list = new List<int> { 1, -15, 30, 60, -50, 40, -55, 60, -45 };
+            int result = list.RemoveAll(v => v < 0);
+            Console.WriteLine(result);
+            foreach (var n in list)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
