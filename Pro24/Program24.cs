71e41ab63d90162edff78f10c624eec5b8a29964
shima <t-shima@softcreate.net>
2026-07-23 12:54:58 +0900

    P552から

--- Pro24/Program24.cs
+++ Pro24/Program24.cs
@@ -2,36 +2,15 @@
 
 namespace Pro24
 {
-    internal partial class MyPartial
-    {
-        public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
-
-        public string Show()
-        {
-            return $"名前は{LastName}{FirstName}です。";
-        }
-    }
-
-    internal partial class MyPartial
-    {
-        public string Greet()
-        {
-            return $"こんにちは、{LastName}{FirstName}さん！";
-        }
-    }
-
     internal class IteratorBasic
     {
         static void Main(string[] args)
         {
-            var mc = new MyPartial
-            {
-                FirstName = "一郎",
-                LastName = "小森田"
-            };
-            Console.WriteLine(mc.Show());
-            Console.WriteLine(mc.Greet());
+            var list = new List<int> { 1, 3, 6, 9 };
+            //list.ForEach(v => Console.WriteLine(v * v));
+            int result = 0;
+            list.ForEach(v => result += v);
+            Console.WriteLine(result);
         }
     }
 }
