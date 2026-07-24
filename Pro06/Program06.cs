e2ff6fdfa28f20aad37435433bfcc02eb95831d6
hiroki <hiroki10484962@gmail.com>
2026-07-24 22:33:12 +0900

    P559から

--- Pro06/Program06.cs
+++ Pro06/Program06.cs
@@ -6,7 +6,17 @@ namespace Pro06
     {
         static void Main(string[] args)
         {
-            
+            foreach (var book in AppTables.Books)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            foreach (var review in AppTables.Reviews)
+            {
+                Console.WriteLine(review);
+            }
         }
     }   
 }
--- Pro07/AppTables.cs
+++ Pro07/AppTables.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Pro06
+namespace Pro07
 {
     internal class Book
     {
