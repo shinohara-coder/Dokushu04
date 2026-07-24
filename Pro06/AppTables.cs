f1948a67b9bc11105ff29f8eee0649d38d08a142
shima <t-shima@softcreate.net>
2026-07-24 12:57:48 +0900

    クラス実装

--- Pro06/AppTables.cs
+++ Pro06/AppTables.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace Pro06
 {
-    internal class AppTables
+    internal class Book
+    {
+        public string Isbn { get; set; } = "";
+        public string Title { get; set; } = "";
+        public int Price { get; set; } = 0;
+        public string Publisher { get; set; } = "";
+        public DateTime Published { get; set; } = DateTime.Today;
+
+        public override string ToString()
+        {
+            return $"{Title}（{Publisher}）{Price}円 {Publisher:d}刊行";
+        }
+    }
+
+    internal class Review
+    {
+        public string Isbn { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Body { get; set; } = "";
+    }
+    internal static class AppTables
     {
 
     }
