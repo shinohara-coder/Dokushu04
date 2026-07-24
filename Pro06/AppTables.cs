c4010a14b7749dfa1266e01516f124e3d6adf173
shima <t-shima@softcreate.net>
2026-07-24 09:38:39 +0900

    AppTables.csを追加

---
+++ Pro06/AppTables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro06
+{
+    internal class AppTables
+    {
+
+    }
+}
--- Pro06/Program06.cs
+++ Pro06/Program06.cs
@@ -2,42 +2,11 @@
 
 namespace Pro06
 {
-    internal class Person
-    {
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
-
-        public Person(string firstName, string lastName, int age)
-        {
-            this.FirstName = firstName;
-            this.LastName = lastName;
-            this.Age = age;
-        }
-
-        public void Deconstruct(out string firstName, out string lastName)
-        {
-            firstName = this.FirstName;
-            lastName = this.LastName;
-        }
-
-        public void Deconstruct(out string firstName, out string lastName, out int age)
-        {
-            firstName = this.FirstName;
-            lastName = this.LastName;
-            age = this.Age;
-        }
-    }
-
     internal class LockBasic
     {
         static void Main(string[] args)
         {
-            var p = new Person("圭太", "山本", 36);
-            var (fn, ln, age) = p;
-            Console.WriteLine(fn);
-            Console.WriteLine(ln);
-            Console.WriteLine(age);
+            
         }
     }   
 }
