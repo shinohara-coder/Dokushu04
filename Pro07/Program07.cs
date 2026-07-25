55ce2c9124aa058f8b688118fb29c7e906a70d42
hiroki <hiroki10484962@gmail.com>
2026-07-25 22:32:28 +0900

    P562から

--- Pro07/Program07.cs
+++ Pro07/Program07.cs
@@ -1,12 +1,18 @@
-﻿namespace Pro07
+﻿using System.Reflection;
+
+namespace Pro07
 {
     internal class AsyncBasic
     {
         static void Main(string[] args)
         {
-            var bs = from b in AppTables.Books
-                     //where b.Price < 3000
-                     select b;
+            //var bs = from b in AppTables.Books
+            //         where b.Price < 3000
+            //         select new { Isbn = b.Isbn ,Title = b.Title, Price = b.Price, Publisher = b.Publisher };
+
+            var bs = AppTables.Books
+                    .Where(b => b.Price < 3000)
+                    .Select(b => new { Title = b.Title, Price = b.Price });
 
             foreach (var b in bs)
             {
