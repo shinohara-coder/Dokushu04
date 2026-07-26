872196c350da4fb04be962323edba3f57bfced3a
shima <t-shima@softcreate.net>
2026-07-27 08:56:10 +0900

    P576から

--- Pro07/Program07.cs
+++ Pro07/Program07.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Reflection.Metadata;
 
 namespace Pro07
 {
@@ -6,21 +7,24 @@ namespace Pro07
     {
         static void Main(string[] args)
         {
-            var bs = from b in AppTables.Books
-                     group b by b.Publisher into pubs
-                     where pubs.Average(b => b.Price) >= 3500
-                     select new
-                     {
-                         Published = pubs.Key,
-                         AveragePrice = pubs.Average(b => b.Price)
-                     };
+            //var bs = from b in AppTables.Books
+            //         group b by b.Publisher into pubs
+            //         where pubs.Average(b => b.Price) >= 1000
+            //         select new
+            //         {
+            //             Published = pubs.Key,
+            //             AveragePrice = pubs.Average(b => b.Price)
+            //         };
 
-            //var bs = AppTables.Books
-            //        .GroupBy(b => new
-            //        {
-            //            Publisher = b.Publisher,
-            //            PublishYear = b.Published.Year
-            //        });
+            var bs = AppTables.Books
+                    .GroupBy(b => b.Publisher)
+                    .Where(pubs => pubs.Average(b => b.Price) >= 3500)
+                    .Select(pubs => new
+                    {
+                        Published = pubs.Key,
+                        AveragePrice = pubs.Average(b => b.Price)
+                    });
+                   
 
             //Console.WriteLine(bs);
             foreach (var b in bs)
