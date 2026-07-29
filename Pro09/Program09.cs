8a11c444f5b2ff607355c158da5f7fa3c70eb8b5
shima <t-shima@softcreate.net>
2026-07-30 08:54:10 +0900

    P590から

--- Pro09/Program09.cs
+++ Pro09/Program09.cs
@@ -7,16 +7,16 @@
             Task t1 = Task.Run(() => Count(1));
             Task t2 = Task.Run(() => Count(2));
             Task t3 = Task.Run(() => Count(3));
-            t1.Wait();
-            t2.Wait();
-            t3.Wait();
+
+            Task.WaitAny(t1, t2, t3);
+            //Task.WaitAll(t1, t2, t3);
 
             Console.WriteLine("すべての処理が終了しました。");
         }
 
         static void Count(int n)
         {
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 Console.WriteLine($"Thread{n}: {i}");
             }
