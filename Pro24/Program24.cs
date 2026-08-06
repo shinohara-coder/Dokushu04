1a59394d768f6c0ae621c670917676b56bd9d6f1
shima <t-shima@softcreate.net>
2026-08-07 08:56:59 +0900

    P75から

--- Pro24/Program24.cs
+++ Pro24/Program24.cs
@@ -6,8 +6,19 @@ namespace Pro24
     {
         static void Main(string[] args)
         {
-            //int? num = null;
-            int num = null;
+            int? num = 108;
+            string varName = nameof(num);
+            //Console.WriteLine(num.HasValue);
+            //Console.WriteLine(num.Value);
+            if (num.HasValue)
+            {
+                int i = num.Value;
+                Console.WriteLine($"変数：{varName}の値は{i}です。");
+            }
+            else
+            {
+                Console.WriteLine($"変数：{varName}は値を持ちません。");
+            }
         }
     }
 }
