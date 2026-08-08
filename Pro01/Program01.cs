5eb2cb1e46edbd6b356bcd737249e0357a75eb27
hiroki <hiroki10484962@gmail.com>
2026-08-08 22:07:04 +0900

    P93から

--- Pro01/Program01.cs
+++ Pro01/Program01.cs
@@ -1,14 +1,42 @@
-﻿namespace SelfCSharp.Chap09.Priority1
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace SelfCSharp.Chap09.Priority1
 {
     internal class LambdaCapture
     {
         static void Main(string[] args)
         {
-            var list = new List<string> { "からすなぜ鳴くの", "からすは山に",
-                 "可愛い七つの", "子があるからよ"};
-            var result = list.Find(str => str.StartsWith("可愛い"));
+            const int Count = 100_000;
 
-            Console.WriteLine(result);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            var sw = Stopwatch.StartNew();
+            var result = "";
+            for (int i = 0; i < Count; i++)
+            {
+                result += "いろは";
+            }
+            sw.Stop();
+            Console.WriteLine($"経過時間1: {sw.ElapsedMilliseconds} ms");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            //sw.Reset();
+            result = "";
+            sw.Restart();
+            var builder = new StringBuilder();
+            for (int i = 0; i < Count; i++)
+            {
+                builder.Append("いろは");
+            }
+            result = builder.ToString();
+            sw.Stop();
+            Console.WriteLine($"経過時間2: {sw.ElapsedMilliseconds} ms");
         }
     }
 }
--- Pro25/Program25.cs
+++ Pro25/Program25.cs
@@ -4,8 +4,21 @@
     {   
         static void Main(string[] args)
         {
-            string? str = null;
-            Console.WriteLine(str);
+            int[][] data = new int[3][];
+            data[0] = new int[] { 11, 12};
+            data[1] = new int[] { 21, 22, 23, 24};
+            data[2] = new int[] { 31, 32, 33};
+
+            for (var i = 0; i < data.Length; i++)
+            {
+                for (var j = 0; j < data[i].Length; j++)
+                {
+                    Console.WriteLine($"data[{i}][{j}] = {data[i][j]}");
+                }
+                Console.WriteLine("--------------------");
+            }
+
+            Console.WriteLine(data.Rank);
         }
     }
 }
