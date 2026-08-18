bd0fc9a3d6ee434710d343c96da8f69b0393888b
hiroki <hiroki10484962@gmail.com>
2026-08-18 22:06:28 +0900

    P298から

--- Pro04/Program04.cs
+++ Pro04/Program04.cs
@@ -1,15 +1,30 @@
 ﻿namespace SelfCSharp.Chap09.Priority1
 {
+    internal class Person
+    {
+        private string _firstName = "";
+        private string _lastName = "";
+
+        public Person(string firstName, string lastName)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+        }
+
+        public Person() : this("権兵衛", "名無") { }
+
+        public void　Show()
+        {
+            Console.WriteLine($"名前は{this._lastName}{this._firstName}です。");
+        }
+    }
     internal class LambdaCapture
     {
         static void Main(string[] args)
         {
-            double Discount(int price, double rate)
-            {
-                return price * (1 - rate);
-            }
-
-            Console.WriteLine(Discount(10000, 0.3));
+            //var p = new Person();
+            var p = new Person("新之助", "及川");
+            p.Show();
         }
     }
 }
--- Pro05/Program05.cs
+++ Pro05/Program05.cs
@@ -1,19 +1,21 @@
 ﻿namespace SelfCSharp.Chap09.Priority1
 {
+    internal class Hoge
+    {
+        public string value = "field";
+        public Hoge() { }
+        public Hoge(string value)
+        {
+            this.value = value;
+        }
+    }
     internal class LambdaCapture
     {
         static void Main(string[] args)
         {
-            object obj = false;
-            Console.WriteLine(obj switch
-            {
-                int i when i >= 15 => "15以上の数値です。",
-                int i => "数値です。",
-                string str when str.Length < 10 => "10文字未満の文字列です。",
-                string str => "文字列です。",
-                bool b => b ? "trueです。" : "falseです。",
-                _ => "意図しない型です。"
-            });
+            var h = new Hoge("constructor") { value = "initializer" };
+
+            Console.WriteLine(h.value);
         }
     }
 }
