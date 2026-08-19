680e1bed7c4bf7c040ca55d6bd23a72ccd7623b0
H_Shinohara <shinohara@softcreate.net>
2026-08-19 10:17:39 +0900

    P302から

--- Pro06/Program06.cs
+++ Pro06/Program06.cs
@@ -1,34 +1,28 @@
-﻿using System.IO.Pipes;
-
-namespace Pro06
+﻿namespace Pro06
 {
+    internal class Circle
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            this._radius = radius;
+        }
+
+        public Circle() : this(1) {}
+
+        public double GetArea()
+        {
+            return Math.PI * this._radius * this._radius;
+        }
+    }
+    
     internal class LockBasic
     {
         static void Main(string[] args)
         {
-            var rank = "甲";
-            #region 一時コメントオフ
-            //switch (rank)
-            //{
-            //    case "甲":
-            //        Console.WriteLine("Excellent!!");
-            //        goto case "丙";
-            //    case "乙":
-            //        Console.WriteLine("Good!");
-            //        goto case "丙";
-            //    case "丙":
-            //        Console.WriteLine("Passed");
-            //        break;
-            //    case "丁":
-            //        Console.WriteLine("Fight!");
-            //        break;
-            //    default:
-            //        Console.WriteLine("????");
-            //        break;
-            //}
-            #endregion
-
-
+            var c = new Circle();
+            Console.WriteLine(c.GetArea());
         }
     }   
 }
