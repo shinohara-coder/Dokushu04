b44177e477d1b1868fc8e5d923a31e967a380213
shima <t-shima@softcreate.net>
2026-08-20 09:56:06 +0900

    P313から

--- Pro12/Program12.cs
+++ Pro12/Program12.cs
@@ -1,24 +1,26 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.CompilerServices;
 
 namespace Pro10
 {
+    internal class MyInitializer
+    {
+        [ModuleInitializer]
+        public static void init()
+        {
+            Console.WriteLine("Init is called!!");
+        }
+
+        [ModuleInitializer]
+        public static void Init2()
+        {
+            Console.WriteLine("Init2 is called!!");
+        }
+    }
     internal class AsuncBasic
     {
         static void Main(string[] args)
         {
-            var str = "自宅の電話番号は、084-111-2222です。携帯は、080-333-4444です。";
-            var rgx = new Regex(@"(?<area>\d{2,4})-(?<city>\d{2,4})-(?<local>\d{4})", RegexOptions.ExplicitCapture);
-
-            var result = rgx.Matches(str);
-            foreach (Match m in result)
-            {
-                Console.WriteLine(m.Value);
-                var gp = m.Groups;
-                Console.WriteLine($"市外局番：{gp["area"]}");
-                Console.WriteLine($"市内局番：{gp["city"]}");
-                Console.WriteLine($"加入者番号：{gp["local"]}");
-                Console.WriteLine();
-            }
+            Console.WriteLine("Main is called!!");
         }
     }
 
