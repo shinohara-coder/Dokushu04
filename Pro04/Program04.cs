5bf629fe9fa5afdd1c5bbacff79f39a5cf2ffc1e
hiroki <hiroki10484962@gmail.com>
2026-08-25 22:06:47 +0900

    P350から

--- Pro04/Program04.cs
+++ Pro04/Program04.cs
@@ -1,30 +1,64 @@
-﻿namespace SelfCSharp.Chap09.Priority1
-{
-    internal class Person
-    {
-        private string _firstName = "";
-        private string _lastName = "";
+﻿using System.Collections;
 
-        public Person(string firstName, string lastName)
+namespace SelfCSharp.Chap09.Priority1
+{
+    internal class PrimeList : IEnumerable<int>
+    {
+        int _max = 2;
+
+        public PrimeList(int max)
         {
-            this._firstName = firstName;
-            this._lastName = lastName;
+            this._max = max;
         }
 
-        public Person() : this("権兵衛", "名無") { }
-
-        public void　Show()
+        public IEnumerator<int> GetEnumerator()
         {
-            Console.WriteLine($"名前は{this._lastName}{this._firstName}です。");
+            bool IsPrime(int value)
+            {
+                var prime = true;
+                var mid = (int)Math.Floor(Math.Sqrt(value));
+                for (var i = 2; i <= mid; i++)
+                {
+                    if (value % i == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                return prime;
+            }
+
+            const int Min = 2;
+
+            if (this._max < Min)
+            {
+                Console.WriteLine("引数maxは2以上の値を指定してください。");
+                yield break;
+            }
+
+            for (var num = Min; num <= this._max; num++)
+            {
+                if (IsPrime(num))
+                {
+                    yield return num;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
     internal class LambdaCapture
     {
         static void Main(string[] args)
         {
-            //var p = new Person();
-            var p = new Person("新之助", "及川");
-            p.Show();
+            var list = new PrimeList(100);
+            foreach (var value in list)
+            {
+                Console.WriteLine(value);
+            }
         }
     }
 }
