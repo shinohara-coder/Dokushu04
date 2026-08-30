f32564523e398f897a5b7cfca0fa87c62a39ba15
H_Shinohara <shinohara@softcreate.net>
2026-08-31 08:55:57 +0900

    P407から

--- Pro21/Program21.cs
+++ Pro21/Program21.cs
@@ -1,18 +1,29 @@
 ﻿namespace Pro21
 {
+    internal class Man
+    {
+
+    }
+
+    internal class BusinessMan : Man
+    {
+
+    }
+
+    internal class StudentMan : Man
+    {
+
+    }
+    
     internal class PassRefArray
     {
-        public int[] Update(ref int[] data)
-        {
-            data = new[] { 10, 20, 30 };
-            return data;
-        }
+        
         static void Main(string[] args)
         {
-            var data = new[] { 2, 4, 6 };
-            var p = new PassRefArray();
-            Console.WriteLine(p.Update(ref data)[0]);
-            Console.WriteLine(data[0]);
+            Man m = new BusinessMan();
+            BusinessMan bm = (BusinessMan)m;
+            StudentMan s = (StudentMan)m;
+            //StudentMan s2 = (StudentMan)bm;
         }
     }
 }
