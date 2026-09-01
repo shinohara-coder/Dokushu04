011daa6a5200e4393f64efa1fa762569c2d60300
H_Shinohara <shinohara@softcreate.net>
2026-09-02 08:53:02 +0900

    子クラス1まで実装

--- Pro25/Program25.cs
+++ Pro25/Program25.cs
@@ -2,12 +2,29 @@
 {
     internal class Figure
     {
+        public double Width { get; set; }
+        public double Height { get; set; }
 
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public virtual double GetArea()
+        {
+            return 0.0;
+        }
     }
 
     internal class Triangle: Figure
     {
+        public Triangle(double width, double height):base(width,height) { }
 
+        public override double GetArea()
+        {
+            return this.Width * this.Height / 2;
+        }
     }
 
     internal class Square: Figure
