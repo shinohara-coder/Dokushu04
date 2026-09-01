5799a232a6293dccc8559de0964e81a454cb8a67
shinohara-coder <shinohara@softcreate.net>
2026-09-01 22:00:18 +0900

    Update Program23.cs

--- Pro23/Program23.cs
+++ Pro23/Program23.cs
@@ -1,10 +1,16 @@
-﻿namespace Pro23
+﻿using Pro23.DateTimeExtension;
+
+namespace Pro23
 {
     internal class PassRefReturn
     {
         static void Main(string[] args)
         {
-           
+            var dt = DateTime.Now;
+            if (dt.IsToday())
+            {
+                Console.WriteLine("今日届いたデータです。");
+            }
         }
     }
 }
