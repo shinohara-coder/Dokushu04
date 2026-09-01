43c12b070ca5f71e42124fd5268ffdf15333f1d3
H_Shinohara <shinohara@softcreate.net>
2026-09-02 08:43:33 +0900

    P407から

--- Pro24/Program24.cs
+++ Pro24/Program24.cs
@@ -1,29 +1,15 @@
-﻿using System.Globalization;
+﻿using Pro24.StringExtensions;
+using System.Globalization;
 
 namespace Pro24
 {
     internal class PassOut
     {
-        public void GetMaxMin(int x, int y, out int max, out int min)
-        {
-            if (x >= y)
-            {
-                max = x;
-                min = y;
-            }
-            else
-            {
-                max = y;
-                min = x;
-            }
-        }
         static void Main(string[] args)
         {
-            var a = new PassOut();
-            int resultMax = 0;
-            int resultMin = 0;
-            a.GetMaxMin(5, 3, out resultMax, out resultMin);
-            Console.WriteLine($"max:{resultMax}\nmin:{resultMin}");
+            //string longTitle = "C#の拡張メソッドについての超分かり易い解説記事";
+            string longTitle = "";
+            Console.WriteLine(longTitle.Truncate(10));
         }
        
     }
--- Pro25/Program25.cs
+++ Pro25/Program25.cs
@@ -1,19 +1,24 @@
 ﻿namespace Pro25.hoge
 {
+    internal class Figure
+    {
+
+    }
+
+    internal class Triangle: Figure
+    {
+
+    }
+
+    internal class Square: Figure
+    {
+
+    }
     internal partial class TupleBasic
     {   
-        public (int max, int min) GetMaxMin(int x, int y)
-        {
-            return x >= y ? (x, y) : (y, x);
-        }
         static void Main(string[] args)
         {
-            var a = new TupleBasic();
-            var t = a.GetMaxMin(100, 250);
-            Console.WriteLine($"{nameof(t.max)}:{t.max}\n{nameof(t.min)}:{t.min}");
-
-            var (outMax, outMin) = a.GetMaxMin(58, 26);
-            Console.WriteLine($"{nameof(outMax)}:{outMax}\n{nameof(outMin)}:{outMin}");
+            
         } 
     }
 }
