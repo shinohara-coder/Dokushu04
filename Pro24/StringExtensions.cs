d1557dbf71f98c117f05d58e9d9786851a6f712e
shinohara-coder <shinohara@softcreate.net>
2026-09-01 22:10:44 +0900

    Create StringExtensions.cs

---
+++ Pro24/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+
+namespace Pro24.StringExtensions
+{
+    internal static class StringExtensions
+    {
+        public static string Truncate(this string str, int maxLength)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            if (str.Length <= maxLength) return str;
+
+            return str.Substring(0, maxLength) + "...";
+        }
+    }
+}
