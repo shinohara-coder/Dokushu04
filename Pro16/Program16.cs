214d91d8269715ed011d71d12b19b9245cea8a76
shinohara-coder <shinohara@softcreate.net>
2026-09-08 22:09:11 +0900

    P479から

--- Pro16/Program16.cs
+++ Pro16/Program16.cs
@@ -10,13 +10,22 @@ namespace Pro16
         {
             this.Name = "名無権兵衛";
         }
+
+        public void Update(string name)
+        {
+            this.Name = name;
+            WriteLine("Update method is finished!!");
+        }
     }
 
     internal class DelegeteUse
     {
+        static readonly MutableValue mv = new();
         static void Main(string[] args)
         {
-            
+            //mv.Name = "佐藤リオ";
+            mv.Update("松本大洋");
+            WriteLine(mv.Name);
         }
     }
 }
