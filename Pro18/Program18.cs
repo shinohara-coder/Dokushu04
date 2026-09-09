e68c9d92d6e3124009f7c8126bc2c2f565ea4e2d
H_Shinohara <shinohara@softcreate.net>
2026-09-09 12:46:45 +0900

    P483から

--- Pro18/Program18.cs
+++ Pro18/Program18.cs
@@ -9,7 +9,12 @@ namespace Pro18
         static void Main(string[] args)
         {
             var p = new Person("一郎", "田中", 10);
-
+            var pc1 = p with { Age = 52 };
+            var pc2 = p with { };
+            WriteLine(pc1);
+            WriteLine(pc2);
+            WriteLine(p == pc2);
+            WriteLine(Object.ReferenceEquals(p, pc2));
         }
     }
 }
