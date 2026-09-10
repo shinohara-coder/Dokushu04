6e41e4d299a2f4d7d224eb5a343a1f2ac2dc8a16
shinohara-coder <shinohara@softcreate.net>
2026-09-10 22:08:13 +0900

    P490から

--- Pro22/Program22.cs
+++ Pro22/Program22.cs
@@ -1,12 +1,33 @@
 ﻿using Pro22.IntExtension;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Pro22
-{   
+{
+    internal class MyClass
+    {
+        //class MyHelper
+        internal class MyHelper
+        {
+            public void Show()
+            {
+                Console.WriteLine("Nested Class is running!");
+            }
+        }
+
+        public void Run()
+        {
+            var helper = new MyHelper();
+            helper.Show();
+        }
+    }
     internal class IteratorBasic
     {
         static void Main(string[] args)
         {
-            5.Times(() => Console.WriteLine("Hello!"));
-        }
+            var c = new MyClass();
+            c.Run();
+
+            var h = new MyClass.MyHelper();
+;        }
     }
 }
--- Pro23/Program23.cs
+++ Pro23/Program23.cs
@@ -2,15 +2,39 @@
 
 namespace Pro23
 {
-    internal class PassRefReturn
+    internal class MyClass
+    {
+        string str1 = "包含・インスタンス";
+        static string str2 = "包含・静的";
+
+        public void Show()
+        {
+            var h = new MyHelper();
+            h.Show();
+            //Console.WriteLine(h.str1);
+            //Console.WriteLine(MyHelper.str2);
+        }
+
+        class MyHelper
+        {
+            string str1 = "入れ子・インスタンス";
+            static string str2 = "入れ子・静的";
+
+            public void Show()
+            {
+                var c = new MyClass();
+                Console.WriteLine(c.str1);
+                Console.WriteLine(MyClass.str2);
+            }
+        }
+    }
+
+    internal class NestedAccess
     {
         static void Main(string[] args)
         {
-            var dt = DateTime.Now;
-            if (dt.IsToday())
-            {
-                Console.WriteLine("今日届いたデータです。");
-            }
+            var mc = new MyClass();
+            mc.Show();
         }
     }
 }
