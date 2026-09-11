77d8e5adee761865f35664a7c12799c2147f2f6e
shinohara-coder <shinohara@softcreate.net>
2026-09-11 22:22:53 +0900

    P495から

---
+++ Pro25/MyPartialMethod1.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Pro25
+{
+    internal partial class MyPartialMethod
+    {
+        static partial void Log()
+        {
+            WriteLine("ログを記録しました。");
+        }
+    }
+}
--- Pro25/Program25.cs
+++ Pro25/Program25.cs
@@ -1,49 +1,12 @@
-﻿namespace Pro25.hoge
+﻿namespace Pro25
 {
-    internal class Figure
+    internal partial class MyPartialMethod
     {
-        public double Width { get; set; }
-        public double Height { get; set; }
-
-        public Figure(double width, double height)
-        {
-            this.Width = width;
-            this.Height = height;
-        }
-
-        public virtual double GetArea()
-        {
-            return 0.0;
-        }
-    }
-
-    internal class Triangle: Figure
-    {
-        public Triangle(double width, double height):base(width,height) { }
-
-        //public override double GetArea()
-        //{
-        //    return this.Width * this.Height / 2;
-        //}
-    }
-
-    internal class Square: Figure
-    {
-        public Square(double width, double height) : base(width, height) { }
-
-        //public override double GetArea()
-        //{
-        //    return this.Width * this.Height;
-        //}
-    }
-    internal partial class TupleBasic
-    {   
+        static partial void Log();
+        
         static void Main(string[] args)
         {
-            Figure t = new Triangle(10, 30);
-            Console.WriteLine(t.GetArea());
-            Figure s = new Square(10, 30);
-            Console.WriteLine(s.GetArea());
+            Log();
         } 
     }
 }
