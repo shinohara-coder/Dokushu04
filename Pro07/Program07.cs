c178c41e553c3c854c0a584172326c2b8245e956
shinohara-coder <shinohara@softcreate.net>
2026-09-14 22:08:18 +0900

    P521から

--- Pro07/Program07.cs
+++ Pro07/Program07.cs
@@ -1,23 +1,73 @@
-﻿using static System.Console;
+﻿using System.Globalization;
+using static System.Console;
 
 namespace Pro07
 {    
+    internal class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public bool Equals(Person? other)
+        {
+            if (Object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (other == null || this.GetType() != other.GetType())
+            {
+                return false;
+            }
+
+            return this.FirstName == other.FirstName && this.LastName == other.LastName;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return this.Equals(obj as Person);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.FirstName.GetHashCode() ^ this.LastName.GetHashCode();
+        }
+
+        public static bool operator ==(Person? p1, Person? p2)
+        {
+            if (Object.ReferenceEquals(p1,p2))
+            {
+                return true;
+            }
+
+            if (p1 is null || p2 is null || p1.GetType() != p2.GetType())
+            {
+                return false;
+            }
+
+            return p1.FirstName == p2.FirstName && p1.LastName == p2.LastName;
+        }
+
+        public static bool operator !=(Person? p1, Person? p2)
+        {
+            return !(p1 == p2);
+        }
+    }
     internal class AsyncBasic
     {
         static void Main(string[] args)
         {
-            try
-            {
-                using (var sr = new StreamReader(@"C:nothing.dat"))
-                {
-                    WriteLine(sr.ReadToEnd());
-                }
-            }
-            catch (FileNotFoundException ex)
-            {
-                WriteLine("ファイルが見つかりませんでした。");
-                WriteLine(ex.StackTrace);
-            }
+            var p1 = new Person("一郎", "田中");
+            var p2 = new Person("一郎", "田中");
+            var p3 = new Person("広樹","篠原");
+            WriteLine(p1 == p2);
+            WriteLine(p1 != p3);
         }
     }
 }
