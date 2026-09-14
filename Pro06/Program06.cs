0e370dc447c121809fcfd50de95cbef4a6cdbd48
H_Shinohara <shinohara@softcreate.net>
2026-09-14 16:30:25 +0900

    P517から

--- Pro06/Program06.cs
+++ Pro06/Program06.cs
@@ -3,13 +3,42 @@ using static System.Math;
 
 namespace Pro06
 {   
+    internal class Person
+    {
+        public string FirstName { get; private set; }
+        public string LasstName { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LasstName = lastName;
+            this.Age = age;
+        }
+
+        public void Deconstruct(out string firstName, out string lastName)
+        {
+            firstName = this.FirstName;
+            lastName = this.LasstName;
+        }
+
+        public void Deconstruct(out string firstName, out string lastName, out int age)
+        {
+            firstName = this.FirstName;
+            lastName = this.LasstName;
+            age = this.Age;
+        }
+    }
     internal class Practice4
     {
-        
         static void Main(string[] args)
         {
-            WriteLine(Abs(-13));
-            WriteLine(Round(1.6));
+            var p = new Person("一郎", "田中", 25);
+            var (fn, ln, age) = p;
+            WriteLine($"名：{fn}　姓：{ln} 年齢：{age}");
+            var p2 = new Person("太郎", "山田", 35);
+            var (fn2, ln2) = p2;
+            WriteLine($"名：{fn2}　姓：{ln2}");
         }
     }   
 }
