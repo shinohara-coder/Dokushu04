996f0b04253d7ccf1158d152226150e1a27fe863
shinohara-coder <shinohara@softcreate.net>
2026-09-15 22:01:36 +0900

    P526から

--- Pro09/Program09.cs
+++ Pro09/Program09.cs
@@ -2,23 +2,40 @@
 
 namespace Pro08
 {
+    internal class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static Coordinate operator ++(Coordinate c)
+        {
+            return new Coordinate()
+            {
+                X = c.X + 1,
+                Y = c.Y + 1
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"({this.X}, {this.Y})";
+        }
+    }
     internal class MySingleton
     {   
         static void Main(string[] args)
         {
-            try
-            {
-                var f = new StreamReader(@"C:\nothing.dat");
-            }
-            catch (FileNotFoundException ex) when (ex.Message.Contains(".dat") || ex.Message.Contains(".txt"))
-            {
-                WriteLine("存在しないファイル.datまたは.txtが指定されました。");
-                WriteLine(ex.Message);
-            }
-            catch (FileNotFoundException)
-            {
-                WriteLine("存在しないファイルが指定されました。");
-            }
+            var a = new Coordinate() { X = 10, Y = 20 };
+            var b = ++a;
+            WriteLine(a);
+            WriteLine(b);
+
+            WriteLine("\n----------------\n");
+
+            var m = new Coordinate() { X = 10, Y = 20 };
+            var n = m++;
+            WriteLine(m);
+            WriteLine(n);
         }
     }
 }
