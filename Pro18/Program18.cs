31d510e634f0bf9a2d2bc2746def485811472934
H_Shinohara <shinohara@softcreate.net>
2026-09-18 12:57:56 +0900

    P546から

--- Pro18/Program18.cs
+++ Pro18/Program18.cs
@@ -2,19 +2,23 @@
 
 namespace Pro18
 {
-    public record Person(string FirstName, string LastName, int Age);
-
-    internal class PassArray
+    internal class DelegateLambda
     {
+        void ArrayWalk(string[] data, Func<string, string> output)
+        {
+            foreach (var value in data)
+            {
+                WriteLine(output(value));
+            }
+        }
         static void Main(string[] args)
         {
-            var p = new Person("一郎", "田中", 10);
-            var pc1 = p with { Age = 52 };
-            var pc2 = p with { };
-            WriteLine(pc1);
-            WriteLine(pc2);
-            WriteLine(p == pc2);
-            WriteLine(Object.ReferenceEquals(p, pc2));
+            var data = new string[] { "あかまきがみ", "あおまきがみ", "きまきがみ" };
+            var dl = new DelegateLambda();
+            dl.ArrayWalk(data, (string d) =>
+                {
+                    return $" [{d}] ";
+                });
         }
     }
 }
