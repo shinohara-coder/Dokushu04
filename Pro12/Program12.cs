ba9f0663ecfaf91aa026b4f9d26cc0f8965badf6
shinohara-coder <shinohara@softcreate.net>
2026-09-26 22:10:08 +0900

    P60から

--- Pro12/Program12.cs
+++ Pro12/Program12.cs
@@ -2,22 +2,14 @@
 
 namespace Pro12
 {
-    delegate void Process(string str);
     internal class LockBasicBad
     {
-        static void Run(string s)
-        {
-            WriteLine($"{s}走ります。");
-        }
         static void Main(string[] args)
         {
-            //var p = new Process(Run);
-            Process p = WriteLine;
-            var strArray = new string[] { "はあはあ", "ちょこちょこ", "すたすた" };
-            foreach (var s in strArray)
-            {
-                p(s);
-            }
+            var name = "山田";
+            WriteLine($@"おはよう、{name}さん！
+                        こんにちは、{name}さん！
+                        さようなら、{name}さん！");
         }
     }
 }
